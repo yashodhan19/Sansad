a88c4f311b39dbfe2915db9a547d5fe1964dc677
Yash <>
2019-06-12 17:42:35 -0500

    Fileoutput
    Fileoutput

--- utils/merge_csv.sh
+++ utils/merge_csv.sh
@@ -1,11 +1,10 @@
 #!/usr/local/bin/sh
 
 CSV_PATH="$1"
-OUTPUT_PATH="$2"
 
 # Ignore header row in files
 for file in $CSV_PATH*; do
     if [ -f "$file" ]; then
-        tail -q -n +2 "$file" | cut -f 1- -d "|" >> "$OUTPUT_PATH"merged.csv
+        tail -q -n +2 "$file" | cut -f 1- -d "|" >> merged.csv
     fi
 done
